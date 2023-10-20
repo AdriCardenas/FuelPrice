e81c01c1f6ad91b63fec7792452df7e059127f52
Adrian Cardenas <tcolacj@gmail.com>
2023-10-20 10:08:04 +0200

    Añadida seleccion de combustible

--- app/src/main/java/com/engineblue/fuelprice/activity/HomeActivity.kt
+++ app/src/main/java/com/engineblue/fuelprice/activity/HomeActivity.kt
@@ -3,10 +3,12 @@ package com.engineblue.fuelprice.activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.engineblue.fuelprice.R
 import com.engineblue.fuelprice.core.ui.AppTheme
 import com.engineblue.fuelprice.screen.ConfigurationFuelScreen
 import com.engineblue.fuelprice.screen.HomeScreen
@@ -24,10 +26,15 @@ class HomeActivity : AppCompatActivity() {
 
         setContent {
             val navController = rememberNavController()
+            val startDestination =
+                getStartDestination()
 
             AppTheme {
 
-                NavHost(navController = navController, startDestination = "onboarding") {
+                NavHost(
+                    navController = navController,
+                    startDestination = startDestination
+                ) {
                     composable("onboarding") {
                         OnboardingScreen {
                             navController.navigate("configuration_fuel") {
@@ -48,6 +55,23 @@ class HomeActivity : AppCompatActivity() {
                 }
             }
 
+        }
+    }
+
+    @Composable
+    private fun getStartDestination(): String {
+        val firstStart =
+            viewModel.getPreferencesFirstStart(getString(R.string.pref_first_start))
+
+        return if (firstStart) {
+            "onboarding"
+        } else {
+            val selectedFuel = viewModel.getSelectedFuel()
+            if (selectedFuel.id != null && selectedFuel.name != null) {
+                "home"
+            } else {
+                "configuration_fuel"
+            }
         }
     }
 }
